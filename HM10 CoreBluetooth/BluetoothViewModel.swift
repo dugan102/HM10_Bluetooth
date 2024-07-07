6919af0f625c1baeb56985b98d819280831d362b
Jackson Dugan <dugan102@d.umn.edu>
2024-07-07 17:04:55 -0500

    first commit

---
+++ HM10 CoreBluetooth/BluetoothViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  BluetoothViewModel.swift
+//  HM10 CoreBluetooth
+//
+//  Created by Jackson Dugan on 7/7/24.
+//
+
+import Foundation
---
+++ HM10 CoreBluetooth/HM10_CoreBluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  HM10_CoreBluetoothApp.swift
+//  HM10 CoreBluetooth
+//
+//  Created by Jackson Dugan on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct HM10_CoreBluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
